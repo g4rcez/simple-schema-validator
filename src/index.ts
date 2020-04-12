4d531b6d43cb2e0e9dcffa5d707eb916b837b419
g4rcez <allan.f.garcez@gmail.com>
2020-04-12 00:52:39 -0300

    feat: export lib attributes and methods

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { schemaValidator as default } from "./schema-validator";
+export { reduceValidations, singleValidator } from "./single-validator";
+export * from "./types";
+export * from "./rules/is-email";
+export * from "./rules/str-validate";
